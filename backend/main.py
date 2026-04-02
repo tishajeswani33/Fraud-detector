b543625557de5018ad6b65ffdb3793161e71b52f
tishajeswani33 <tishajeswani3@outlook.com>
2026-04-02 21:38:01 +0530

    fix: allow all CORS origins

--- backend/main.py
+++ backend/main.py
@@ -76,7 +76,7 @@ ALLOW_ORIGINS = os.getenv(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOW_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
